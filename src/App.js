43e01325b65fbcdb5139622091116c6611d581ed
Manmohan Singh Hunjan <71703296+manmohanhunjan@users.noreply.github.com>
2023-07-26 22:21:33 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+import { Greet } from "./components/Greet";
+import Welcome from "./components/welcome";
+import Hello from "./components/Hello";
+import Message from "./components/Message";
+import Counter from "./components/Counter";
+import Destructuring from "./components/Destructuring";
+import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
+import EventBind from "./components/EventBind";
+import ParentComponent from "./components/ParentComponent";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ParentComponent />
+    {/* <EventBind /> */}
+      {/* <FunctionClick />
+      <ClassClick /> */}
+    {/* <Counter />
+    <Destructuring name="Bruce" heroName="Batman"/> */}
+    {/* <Message/> */}
+      {/* <Greet name="Bruce" heroName="Batman">
+        <p>This is children props</p>
+      </Greet>
+      <Greet name="Clark" heroName="Superman" />
+      <button>Action</button>
+      <Greet name="Diana" heroName="Wonder Woman" />
+
+      <Welcome name="Bruce" heroName="Batman" />
+      <Welcome name="Clark" heroName="Superman" />
+      <Welcome name="Diana" heroName="Wonder Woman" /> */}
+      
+      {/* <Welcome />
+      <Hello /> */}
     </div>
   );
 }
